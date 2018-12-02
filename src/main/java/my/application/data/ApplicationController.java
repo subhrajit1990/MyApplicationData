0ef60f3423b81ad8ca64db853509d44e149ef0c6
subhrajit1990 <subhrajitsahu1990@gmail.com>
2018-12-02 09:30:38 +0530

    initinal commit

---
+++ src/main/java/my/application/data/ApplicationController.java
@@ -0,0 +1,12 @@
+package my.application.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationController {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationController.class, args);
+	}
+}
